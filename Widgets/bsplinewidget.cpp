b008ce57d542f0caa2fbdd4e1d252e17c614e657
Jonas Andersen <jonas.bjorge.andersen@gmail.com>
2019-11-02 14:36:59 +0100

    Displaying points in Widget

--- Widgets/bsplinewidget.cpp
+++ Widgets/bsplinewidget.cpp
@@ -11,7 +11,13 @@ BSplineWidget::BSplineWidget(Entity entityIn, QWidget *parent) :
 
     component_ = getWorld()->getComponent<BSpline>(entityIn).value();
 
-    ui->startPos->setText(component_->curve_.getControlPoints().at(0).toQString());
+    ui->startPos->setText(component_->curve_.getControlPoints().front().toQString());
+    ui->endPos->setText(component_->curve_.getControlPoints().back().toQString());
+
+    for(auto it = component_->curve_.getControlPoints().begin() + 1; it != component_->curve_.getControlPoints().end() - 1; ++it)
+    {
+        ui->pointList->addItem(it->toQString());
+    }
 }
 
 BSplineWidget::~BSplineWidget()
